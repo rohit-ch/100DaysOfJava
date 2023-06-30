fb08f0b5b7f5f8ad62c7554432bfb16218a487a5
Rohit Chaturvedi <rchaturvedi450@gmail.com>
2023-06-30 21:59:54 +0530

    Day two of java with Enums

---
+++ two/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Color myColor = Color.Red;
+        System.out.println(myColor.getHexValue());
+
+        try {
+            myColor.getColorFromHexCode("#002344");
+        } catch (EnumConstantNotPresentException e) {
+            System.out.println(e.getLocalizedMessage());
+        }
+
+        System.out.println(myColor.getColorFromHexCode("#FFFF00"));
+        System.out.println(myColor.getColorFromHexCode("#00ff00"));
+    }
+}
